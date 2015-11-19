be2f4e5c069a7a298ffc1f05c9d031c73024a98c
AkitaOnRails <fabioakita@gmail.com>
2015-11-19 19:21:40 -0200

    adding session management

--- test/views/layout_view_test.exs
+++ test/views/layout_view_test.exs
@@ -1,3 +1,31 @@
 defmodule Pxblog.LayoutViewTest do
   use Pxblog.ConnCase, async: true
+  alias Pxblog.LayoutView
+  alias Pxblog.User
+
+  setup do
+    User.changeset(%User{}, %{username: "test", password: "test", password_confirmation: "test", email: "test@test.com"})
+    |> Repo.insert
+    conn = conn()
+    {:ok, conn: conn}
+  end
+
+  test "current user returns the user in the session", %{conn: conn} do
+    conn = post conn, session_path(conn, :create), user: %{username: "test", password: "test"}
+    assert LayoutView.current_user(conn)
+  end
+
+  test "current user returns nothing if there is no user in the session", %{conn: conn} do
+    user = Repo.get_by(User, %{username: "test"})
+    conn = delete conn, session_path(conn, :delete, user)
+    refute LayoutView.current_user(conn)
+  end
+
+  test "deletes the user session if it exists", %{conn: conn} do
+    user = Repo.get_by(User, %{username: "test"})
+    conn = delete conn, session_path(conn, :delete, user)
+    refute get_session(conn, :current_user)
+    assert get_flash(conn, :info) == "Signed out successfully!"
+    assert redirected_to(conn) == page_path(conn, :index)
+  end
 end
--- web/router.ex
+++ web/router.ex
@@ -19,6 +19,7 @@ defmodule Pxblog.Router do
     get "/", PageController, :index
     resources "/posts", PostController
     resources "/users", UserController
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
   end
 
   # Other scopes may use custom stacks.
